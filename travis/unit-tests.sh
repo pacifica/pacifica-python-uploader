10bdb8718bf854b5e8e487c10e8456efdf1d07a4
dmlb2000 <dmlb2000@gmail.com>
2017-07-07 09:29:55 -0700

    add all the travis scripts

---
+++ travis/unit-tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export POSTGRES_ENV_POSTGRES_USER=postgres
+export POSTGRES_ENV_POSTGRES_PASSWORD=
+coverage run --include='metadata/*' -m pytest -v
+coverage report --show-missing --fail-under 100
+if [[ $CODECLIMATE_REPO_TOKEN ]] ; then
+  codeclimate-test-reporter
+fi
