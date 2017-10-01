7a46e165d7c67157613284d81ca3186f70feec77
David Brown <dmlb2000@gmail.com>
2017-09-30 19:30:42 -0700

    add auth hash to uploader (#26)

--- uploader/Uploader.py
+++ uploader/Uploader.py
@@ -46,12 +46,12 @@ class Uploader(CommonBase):
         headers = {'content-type': 'application/octet-stream'}
         if content_length:
             headers['content-length'] = str(content_length)
-        resp = self.session.post(self._upload_url, data=read_fd, headers=headers)
+        resp = self.session.post(self._upload_url, data=read_fd, headers=headers, **self._auth)
         LOGGER.debug('Upload Resp %s', resp.json())
         return resp.json()['job_id']
 
     def getstate(self, job_id):
         """Get the ingest state for a job."""
-        resp = self.session.get('{}?job_id={}'.format(self._status_url, job_id))
+        resp = self.session.get('{}?job_id={}'.format(self._status_url, job_id), **self._auth)
         LOGGER.debug('Status Resp %s', resp.json())
         return resp.json()
