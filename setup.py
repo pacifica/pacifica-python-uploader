f2fbb708fe28935bd7f6fc54958c3d19f2d8ce96
David Brown <dmlb2000@gmail.com>
2020-05-15 10:22:18 -0700

    Remove Requirements (#60)
    This removes the requirements file and puts the dependencies into
    setup.py so we can use Pip>=20. This also adds documentation testing
    and updates pre-commit config
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- setup.py
+++ setup.py
@@ -2,14 +2,8 @@
 # -*- coding: utf-8 -*-
 """Setup and install the metadata."""
 from os import path
-try:  # pip version 9
-    from pip.req import parse_requirements
-except ImportError:
-    from pip._internal.req import parse_requirements
 from setuptools import setup, find_packages
 
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-INSTALL_REQS = parse_requirements('requirements.txt', session='hack')
 
 setup(
     name='pacifica-uploader',
@@ -25,5 +19,5 @@ setup(
     author_email='david.brown@pnnl.gov',
     packages=find_packages(include=['pacifica.*']),
     namespace_packages=['pacifica'],
-    install_requires=[str(ir.req) for ir in INSTALL_REQS]
+    install_requires=['requests']
 )
